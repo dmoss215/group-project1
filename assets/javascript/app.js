70f1f008b7ca558885ce65e16f9b52b5fb040441
Daniel Moss <dmoss@stratoswp.com>
2017-12-10 20:45:11 -0500

    working signup modal with firebase auth

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,26 +26,46 @@ $('.modal').modal();
 // ===== Collapse Navbar ========
 $(".button-collapse").sideNav();
 
+
+$("#signup-button").on('click', function () { 
+	console.log("sign up button worked");
+	email = $("#signup-email").val().trim();
+	password = $("#signup-pw").val().trim();
+
+	console.log(email, password);
+	firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+		// Handle Errors here.
+		var errorCode = error.code;
+		var errorMessage = error.message;
+		// ...
+	  });
+	  $("#signup-email").val("");
+	  $("#signup-pw").val("");
+ })
+
 //firebase authentication
-var email;
-var password;
-var emailOffered = $('#email-input').val().trim();
-var passwordOffered = $('#password-input').val().trim();
+// var email;
+// var password;
+// var emailOffered = $("#email-address").val().trim();
+// var passwordOffered = $('#password-input').val().trim();
 
 //firebase auth listener
 firebase.auth().onAuthStateChanged(function(user) {
  window.user = user; // user is undefined if no user signed in
 });
 
-$('#register-button').on('click', function(){
+$('#signup').on('click', function(){
 
+	console.log(emailOffered);
 	firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
   	// Handle Errors here.
   	email = emailOffered;
   	password = passwordOffered;
   	var errorCode = error.code;
   	var errorMessage = error.message;
-  	// ...
+	  // ...
+	console.log(email);
+	
 	});	
 });	
 
