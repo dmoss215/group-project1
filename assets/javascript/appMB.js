f39e0d2c745884efd74fd5c97a56bc853daa8aca
Maria Baker <maria.baker.bsee@outlook.com>
2017-12-12 18:34:57 -0500

    Working search history firebase.

--- assets/javascript/appMB.js
+++ assets/javascript/appMB.js
@@ -1,6 +1,4 @@
-
-
-  // Initialize Firebase
+// Initialize Firebase
   var config = {
     apiKey: "AIzaSyCUE8vxN9tFrV5spAnE3AvkWPYNA-ai24k",
     authDomain: "group-project-1-f1ac1.firebaseapp.com",
@@ -9,19 +7,40 @@
     storageBucket: "group-project-1-f1ac1.appspot.com",
     messagingSenderId: "891913964044"
   };
-
   firebase.initializeApp(config);
 
-var productSearch = "";
+  var database = firebase.database();
+  //Initial value.
+  //var prodSearch = "shampoo";
+  //console.log(prodSearch);
+  // Capture Button Click
+  $("#submit-button").on("click", function (event){
+       event.preventDefault();
+  
+      // Values input in text boxes.
+      var prodSearch = $("#video-search").val().trim();
+      console.log(prodSearch);
+      // Code for "Setting values in the database"
+      database.ref().set({
+        
+      prodSearch: prodSearch 
+  });
 
-$("#submit-button").on("click", function(){
+});
+//===========================================================================================
+    // Firebase watcher + initial loader HINT: .on("value")
+    database.ref().on("value", function(snapshot) {
 
-  productSearch = $("#video-search").val().trim();
+      // Log snapshot
+      //console.log(snapshot.val());
+      console.log(snapshot.val());
 
-  firebase.database().ref().set({
 
-    productSearch: productSearch
-    
-    })
+      // Change the HTML to reflect
+      $("#video-search").text(snapshot.val().prodSearch);
 
-  })
+      // Handle the errors
+    }, function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+    });
+ 
